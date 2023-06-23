30effa976d2dee6c3bbf1362c9c940ee9c8adf4a
KatanES <124148302+KatanES@users.noreply.github.com>
2023-06-23 20:53:39 +0000

    Deploying to gh-pages from @ KatanES/goit-js-hw-09@62634613550135699e4e3eb217e4af93de3da2b4 🚀

---
+++ 01-color-switcher.6c63f73e.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var t,e=document.body,n=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");function a(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}n.addEventListener("click",function(){t=setInterval(a,1e3)}),o.addEventListener("click",function(){clearInterval(t)})});
+//# sourceMappingURL=01-color-switcher.6c63f73e.js.map
