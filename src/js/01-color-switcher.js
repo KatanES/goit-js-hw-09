62634613550135699e4e3eb217e4af93de3da2b4
KatanES <businesskatan.es@gmail.com>
2023-06-23 23:52:41 +0300

    half of the 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+  const body = document.body;
+  const changeColorButton = document.querySelector('[data-start]');
+  const stopColorButton = document.querySelector('[data-stop]');
+  let intervalId;
+
+  function changeBackgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+  }
+
+  changeColorButton.addEventListener('click', function () {
+    intervalId = setInterval(changeBackgroundColor, 1000);
+  });
+
+  stopColorButton.addEventListener('click', function () {
+    clearInterval(intervalId);
+  });
+});
