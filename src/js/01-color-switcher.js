6e067238af9364e6cf33d93fa220032bbd4fd211
KatanES <businesskatan.es@gmail.com>
2023-06-24 16:34:43 +0300

    2nd try switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   const body = document.body;
-  const changeColorButton = document.querySelector('[data-start]');
-  const stopColorButton = document.querySelector('[data-stop]');
-  let intervalId;
+  const changeColorButton = document.querySelector('button[data-start]');
+  const stopColorButton = document.querySelector('button[data-stop]');
+  let intervalId = null;
+  let isActive = false;
 
   function changeBackgroundColor() {
+    if (isActive) {
+      return;
+    }
     body.style.backgroundColor = getRandomHexColor();
   }
 
-  changeColorButton.addEventListener('click', function () {
-    intervalId = setInterval(changeBackgroundColor, 1000);
-  });
+  function startColorChange() {
+    if (!isActive) {
+      isActive = true;
+      intervalId = setInterval(changeBackgroundColor, 1000);
+      changeColorButton.disabled = true;
+    }
+  }
 
-  stopColorButton.addEventListener('click', function () {
-    clearInterval(intervalId);
-  });
+  function stopColorChange() {
+    if (isActive) {
+      isActive = false;
+      clearInterval(intervalId);
+      changeColorButton.disabled = false;
+    }
+  }
+
+  changeColorButton.addEventListener('click', startColorChange);
+
+  stopColorButton.addEventListener('click', stopColorChange);
 });
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,114 @@
 import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+
+// const input = document.getElementById('datetime-picker');
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     const selectedDate = selectedDates[0];
+//     if (selectedDate < new Date()) {
+//       window.alert('Please choose a date in the future');
+//       changeColorButton.disabled = true;
+//     } else {
+//       changeColorButton.disabled = false;
+//     }
+//   },
+// };
+
+// flatpickr(input, options);
+const startBtn = document.getElementById('start-btn');
+
+const picker = flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const now = new Date();
+
+    if (selectedDate <= now) {
+      window.alert('Please choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+});
+
+// Start button click event handler
+startBtn.addEventListener('click', () => {
+  // Start the countdown
+  // ...
+
+  // Example code to update timer values every second (replace with your logic)
+  const timerValues = {
+    days: 1,
+    hours: 2,
+    minutes: 30,
+    seconds: 45,
+  };
+
+  updateTimer(timerValues);
+});
+
+const refs = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+// Function to update timer values
+function updateTimer(values) {
+  refs.days.textContent = formatValue(values.days);
+  refs.hours.textContent = formatValue(values.hours);
+  refs.minutes.textContent = formatValue(values.minutes);
+  refs.seconds.textContent = formatValue(values.seconds);
+}
+
+// Helper function to format timer values with leading zeros
+function formatValue(value) {
+  return value.toString().padStart(2, '0');
+}
+
+// const refs = {
+//   startBtn: document.querySelector('button[data-start]'),
+//   timerFace: document.querySelector('.timer'),
+// };
+
+// const timer = {
+//   intervalId: null,
+//   isActive: false,
+//   start() {
+//     if (isActive) {
+//       return;
+//     }
+//     const startTime = Date.now();
+
+//     setInterval(() => {
+//       const currentTime = Date.now();
+//       const deltaTime = currentTime - startTime;
+//       const { hours, mins, secs } = getTimeComponents(deltaTime);
+
+//       console.log(`${hours} : ${min} : ${secs}`);
+//     }, 1000);
+//   },
+//   stop() {
+//     clearInterval();
+//   },
+// };
+
+// refs.startBtn.addEventListener('click', () => {
+//   timer.start();
+// });
+
+// function updateClockFace({ hours, min, secs }) {
+//   refs.timerFace.textContent = `${hours} : ${min} : ${secs}`;
+// }
+// // (Обновляет значения в интерфейсе)
