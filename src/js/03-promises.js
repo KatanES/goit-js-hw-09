46dbb1f515b5216ac5ee4bdf59ec85975cf7b4a7
KatanES <businesskatan.es@gmail.com>
2023-06-25 14:29:07 +0300

    Revert "task 3"
    This reverts commit dac10679e10b5e8e0849b3d6e8139378b92fe609.

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,45 +1,8 @@
-import Notiflix from 'notiflix';
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-}
-
-const form = document.querySelector('.form');
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  const delayInput = this.elements.delay;
-  const stepInput = this.elements.step;
-  const amountInput = this.elements.amount;
-
-  const delay = Number(delayInput.value);
-  const step = Number(stepInput.value);
-  const amount = Number(amountInput.value);
-
-  for (let i = 1; i <= amount; i++) {
-    createPromise(i, delay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.Success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.Failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
-      });
-
-    delay += step;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
   }
-
-  this.reset();
-});
+}
