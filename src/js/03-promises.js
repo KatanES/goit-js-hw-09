3570d908b60a8a77795fbf59aabd34b4c567673a
KatanES <businesskatan.es@gmail.com>
2023-06-25 14:30:54 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,42 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = this.elements.delay;
+  const stepInput = this.elements.step;
+  const amountInput = this.elements.amount;
+
+  const delay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay += step;
+  }
+
+  this.reset();
+});
